3160d5e70c47177ca0ffe865b102af79295e90cc
Duc Nguyen <minhduc140583@gmail.com>
2021-11-21 22:27:02 +0700

    Upgrade version

--- src/context.ts
+++ src/context.ts
@@ -2,7 +2,7 @@ import { HealthController, resources } from 'express-ext';
 import { Pool } from 'mysql';
 import { MySQLChecker, param, PoolManager } from 'mysql-core';
 import { mysql, SearchBuilder } from 'query-core';
-import { createValidator } from 'validator-x';
+import { createValidator } from 'xvalidators';
 import { SqlUserService, User, UserController, UserFilter, userModel } from './user';
 
 resources.createValidator = createValidator;
@@ -20,8 +20,7 @@ export function createContext(pool: Pool): ApplicationContext {
   const userService = new SqlUserService(userSearchBuilder.search, param, manager.query, manager.exec);
   const user = new UserController(log, userService);
 
-  const ctx: ApplicationContext = { health, user };
-  return ctx;
+  return { health, user };
 }
 export function log(msg: string, ctx?: any): void {
   console.log(msg);
