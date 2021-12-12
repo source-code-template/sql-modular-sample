86e74b6e4795c5f60bf296b3bb04d07601631113
Duc Nguyen <minhduc140583@gmail.com>
2021-12-12 23:31:29 +0700

    Check type at middleware

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,4 +1,4 @@
-import { DateRange, Filter, Model, Repository, Service } from 'onecore';
+import { Attributes, DateRange, Filter, Repository, Service } from 'onecore';
 
 export interface UserFilter extends Filter {
   id: string;
@@ -19,31 +19,27 @@ export interface UserRepository extends Repository<User, string> {
 export interface UserService extends Service<User, string, UserFilter> {
 }
 
-export const userModel: Model = {
-  name: 'user',
-  table: 'users',
-  attributes: {
-    id: {
-      key: true,
-      length: 40
-    },
-    username: {
-      required: true,
-      length: 255
-    },
-    email: {
-      format: 'email',
-      required: true,
-      length: 120
-    },
-    phone: {
-      format: 'phone',
-      required: true,
-      length: 14
-    },
-    dateOfBirth: {
-      column: 'date_of_birth',
-      type: 'datetime'
-    }
+export const userModel: Attributes = {
+  id: {
+    key: true,
+    length: 40
+  },
+  username: {
+    required: true,
+    length: 255
+  },
+  email: {
+    format: 'email',
+    required: true,
+    length: 120
+  },
+  phone: {
+    format: 'phone',
+    required: true,
+    length: 14
+  },
+  dateOfBirth: {
+    column: 'date_of_birth',
+    type: 'datetime'
   }
 };
