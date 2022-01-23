71412531b57bbd05d0f6ac19182b3c1238152c55
Duc Nguyen <minhduc140583@gmail.com>
2022-01-23 15:46:33 +0700

    Refactor sql

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import { json } from 'body-parser';
 import { merge } from 'config-plus';
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { json } from 'express';
 import { MiddlewareLogger } from 'express-ext';
 import http from 'http';
 import { createLogger } from 'logger-core';
@@ -20,17 +19,9 @@ const middleware = new MiddlewareLogger(logger.info, conf.middleware);
 app.use(json(), middleware.log);
 
 const pool = mysql.createPool(conf.db);
-
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error('Failed to connect to MySQL.', err.message, err.stack);
-  } else if (conn) {
-    console.log('Connected successfully to MySQL.');
-    const db = new PoolManager(pool);
-    const ctx = useContext(db, logger, middleware);
-    route(app, ctx);
-    http.createServer(app).listen(conf.port, () => {
-      console.log('Start server at port ' + conf.port);
-    });
-  }
+const db = new PoolManager(pool);
+const ctx = useContext(db, logger, middleware);
+route(app, ctx);
+http.createServer(app).listen(conf.port, () => {
+  console.log('Start server at port ' + conf.port);
 });
