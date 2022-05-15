9d36b713da3d28fae1beb81cd81d02769e49465a
Duc Nguyen <minhduc140583@gmail.com>
2022-05-15 23:50:39 +0700

    Apply new version

--- src/user/user-controller.ts
+++ src/user/user-controller.ts
@@ -2,7 +2,7 @@ import { Controller, Log } from 'express-ext';
 import { User, UserFilter, UserService } from './user';
 
 export class UserController extends Controller<User, string, UserFilter> {
-  constructor(log: Log, userService: UserService) {
-    super(log, userService);
+  constructor(log: Log, service: UserService) {
+    super(log, service);
   }
 }
